f2c5b391ec5fa281a6de69fa45b07c3a87e5d425
husseinizaid <husseinizaid6@gmail.com>
2023-09-13 17:37:01 -0400

    Created utils_tests.py and utils.py

---
+++ utils.py
@@ -0,0 +1,23 @@
+class utils:
+
+    def reversed(number):
+        if (isinstance(number, int)):
+            number = int(number)
+            reversed = 0
+            while number > 0:
+                lastDigit = number % 10
+                reversed = reversed * 10 + lastDigit
+                number = number // 10
+            return reversed
+        else:
+            return "Please type an integer!"
+        
+
+    def formatter(number):
+        if (isinstance(number, int)):
+            number = int(number)
+            return (bin(number), oct(number))
+        else:
+            return "Please type an integer!"
+
+
---
+++ utils_tests.py
@@ -0,0 +1,11 @@
+import utils
+
+utilsObject =  utils.utils
+
+print(utilsObject.reversed("Hello"))
+print(utilsObject.reversed(5.232))
+print(utilsObject.reversed(987654321))
+
+print(utilsObject.formatter("Hello"))
+print(utilsObject.formatter(5.232))
+print(utilsObject.formatter(987654321))
