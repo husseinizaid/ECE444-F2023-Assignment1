40e4039405e5e21e0fd65a1c1eee0ee11342f1c4
husseinizaid <husseinizaid6@gmail.com>
2023-09-13 17:46:40 -0400

    created utils_test.py

--- utils_tests.py
+++ utils_tests.py
@@ -2,10 +2,10 @@ import utils
 
 utilsObject =  utils.utils
 
-print(utilsObject.reversed("Test"))
+print(utilsObject.reversed("Tester String"))
 print(utilsObject.reversed(5.232))
 print(utilsObject.reversed(987654321))
 
-print(utilsObject.formatter("Test"))
+print(utilsObject.formatter("Test String"))
 print(utilsObject.formatter(5.232))
 print(utilsObject.formatter(987654321))
